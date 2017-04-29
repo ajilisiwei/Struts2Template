4aba999e9706dfa4998f13c7e57da2a748af314b
ajilisiwei <ajilisiwei@126.com>
2017-04-29 14:07:38 +0800

    新增Maven的struts项目

---
+++ StudentRegistDemo/src/main/java/com/wei/action/HelloWorldAction.java
@@ -0,0 +1,22 @@
+package com.wei.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloWorldAction extends ActionSupport{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		return SUCCESS;
+	}
+
+	public String regist() {
+		System.out.println("成功");
+		return "success";
+	}
+}
